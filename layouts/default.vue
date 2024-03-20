6edf9c14b80bce36529ca1441fda7851c63ceb61
william galas <wgalas@centralizedinc.com>
2024-03-20 22:37:34 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<script setup>
+    const links = [{
+        label: 'Docs',
+        icon: 'i-heroicons-book-open',
+        to: '/getting-started'
+        }, {
+        label: 'Pro',
+        icon: 'i-heroicons-square-3-stack-3d',
+        to: '/pro'
+        }, {
+        label: 'Releases',
+        icon: 'i-heroicons-rocket-launch',
+        to: '/releases'
+        }]
+</script>
+<template>
+    <UHeader :links="links">
+        <template #logo>
+            <h1>PANDO APP</h1>
+        </template>
+        <template #right>
+        <UColorModeButton />
+        <UButton to="https://github.com/nuxt/ui" target="_blank" icon="i-simple-icons-github" color="gray" variant="ghost" />
+        </template>
+
+    </UHeader>
+    <NuxtPage />
+</template>
